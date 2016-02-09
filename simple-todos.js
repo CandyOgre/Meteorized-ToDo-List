e5aab7002bd41a890af756ba88c2a2420f053d87
CandyOgre <no.one.coin@gmail.com>
2016-02-09 08:55:06 +0200

    Remove insecure package. Add Meteor methods for use in client

--- simple-todos.js
+++ simple-todos.js
@@ -54,6 +54,26 @@ if(Meteor.isClient) {
 
 }
 
+Meteor.methods({
+  addTask: function(text) {
+    if(!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId,
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  }, 
+  setChecked: function(taskId, setChecked) {
+    Tasks.update(taskId, {$set: {checked: setChecked} });
+  }
+});
+
 if(Meteor.isServer) {
 
 }
