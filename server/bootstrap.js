09c9ca08dc70608a0c4906b16646dac2ca52c86c
CandyOgre <no.one.coin@gmail.com>
2016-03-15 18:31:03 +0200

    Add some initialization data

---
+++ server/bootstrap.js
@@ -0,0 +1,27 @@
+if(Tasks.find().count() === 0) {
+    Tasks.insert({
+      text: 'Take out the trash',
+      createdAt: new Date(),
+      owner: null,
+      username: 'Miki',
+      private: false,
+      checked: false
+    });
+    Tasks.insert({
+      text: 'Walk with a dog',
+      createdAt: new Date(),
+      owner: null,
+      username: 'Antonio',
+      private: false,
+      checked: false
+    });
+    Tasks.insert({
+      text: 'Buy some carrot',
+      createdAt: new Date(),
+      owner: null,
+      username: 'Antonio',
+      private: false,
+      checked: false
+    });
+
+}
