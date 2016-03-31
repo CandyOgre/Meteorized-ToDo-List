9cbd5fe9d0fc552df9c3f47e7790fe392bae8b49
CandyOgre <no.one.coin@gmail.com>
2016-03-31 09:21:23 +0300

    Add incomplete counter

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -12,7 +12,16 @@ Template.body.onCreated(function bodyOnCreated() {
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({}, {sort: {createdAt: -1} });
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -26,5 +35,8 @@ Template.body.events({
     });
 
     e.target.text.value = "";
+  },
+  'change .hide-completed input'(e, instance) {
+    instance.state.set('hideCompleted', e.target.checked);
   },
 });
